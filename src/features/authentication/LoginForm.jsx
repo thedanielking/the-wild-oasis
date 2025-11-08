d57ff48cdf0c2208432c8b519ccf30ac7035cc7b
Daniel King <dking9486@gmail.com>
2025-11-08 18:10:35 +0100

    authentication 2commit

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("user@gmail.com");
+  const [password, setPassword] = useState("userpassword");
 
   const {login, isLoading} = useLogin();
 
